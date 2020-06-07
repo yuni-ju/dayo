fe875a8f902f9cf5d1ed6ced573468f7a2b75e5c
gemiJ <30407907+gemiJ@users.noreply.github.com>
2020-06-08 03:18:59 +0900

    upload
    join / login / pwdchange complete

--- app/src/main/java/com/joo/dayo/LogInActivity.java
+++ app/src/main/java/com/joo/dayo/LogInActivity.java
@@ -4,6 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,17 +36,20 @@ public class LogInActivity extends Activity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        //로그인 기능
         loginBtn = (Button) findViewById(R.id.loginBtn);
+        emailTxt = (EditText) findViewById(R.id.emailTxt);
+        pwdTxt = (EditText) findViewById(R.id.pwdTxt);
+        joinText = (TextView) findViewById(R.id.joinText);
+        emailTxt = (EditText) findViewById(R.id.emailTxt);
+        pwdTxt = (EditText) findViewById(R.id.pwdTxt);
+        pwdUpdateTxt = (TextView) findViewById(R.id.pwdUpdateTxt);
+
+        //로그인 기능
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                emailTxt = (EditText) findViewById(R.id.emailTxt);
-                pwdTxt = (EditText) findViewById(R.id.pwdTxt);
                 final String email = emailTxt.getText().toString();
                 final String pwd = pwdTxt.getText().toString();
-
                 if (email.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "이메일을 입력하세요.", Toast.LENGTH_SHORT).show();
                 } else if (pwd.isEmpty()) {
@@ -55,41 +61,50 @@ public class LogInActivity extends Activity {
         });
 
         //회원가입 버튼 클릭
-        joinText = (TextView) findViewById(R.id.joinText);
         joinText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                emailTxt = (EditText) findViewById(R.id.emailTxt);
-                pwdTxt = (EditText) findViewById(R.id.pwdTxt);
                 Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
                 startActivityForResult(intent, 0);
             }
         });
-/*
+
         //비밀번호 재설정 기능
-        pwdUpdateTxt = (TextView) findViewById(R.id.joinText);
-        pwdTxt.setOnClickListener(new View.OnClickListener() {
+        pwdUpdateTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                emailTxt = (EditText) findViewById(R.id.emailTxt);
                 final String email = emailTxt.getText().toString();
-
-                mAuth = FirebaseAuth.getInstance();
-                String emailAddress = email;
-
-                mAuth.sendPasswordResetEmail(emailAddress)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if(task.isSuccessful()){
-
+                if (email.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "이메일을 입력하세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    new AlertDialog.Builder(LogInActivity.this) // TestActivity 부분에는 현재 Activity의 이름 입력.
+                            .setMessage(email + " 로 비밀번호 재설정 메일을 전송하시겠습니까?")     // 제목 부분 (직접 작성)
+                            .setPositiveButton("예", new DialogInterface.OnClickListener() {      // 버튼1 (직접 작성)
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //재설정 메일 전송
+                                    mAuth.sendPasswordResetEmail(email)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if (task.isSuccessful()) {
+                                                        Toast.makeText(getApplicationContext(), "이메일을 전송했습니다.", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                    else{
+                                                        Toast.makeText(getApplicationContext(), "해당 계정이 없습니다.", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                }
+                                            });
                                 }
-                            }
-                });
+                            })
+                            .setNegativeButton("아니오", new DialogInterface.OnClickListener() {     // 버튼2 (직접 작성)
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(getApplicationContext(), "취소", Toast.LENGTH_SHORT).show(); // 실행할 코드
+                                }
+                            })
+                            .show();
+                }
             }
         });
-*/
-
     }
 
     private void logIn(String email, String pwd) {
